d514ffb0289f25938a73802af82858c99ae87062
Dan Kang <dankang27@gmail.com>
2018-06-21 17:05:03 -0400

    Set up React

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>splendar</h1>
+  </div>
+);
+
+export default App;
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,8 +1,8 @@
 import merge from "lodash/merge";
 
-import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+// import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
-// DKVNG: need to hard code a user, also maybe get all events when current user fetch?? 
+// DKVNG: need to hard code a user, also maybe get all events when current user fetch??
 
 const _nullUser = Object.freeze({
   currentUser: null
@@ -11,9 +11,9 @@ const _nullUser = Object.freeze({
 const sessionReducer = (state = _nullUser, action) => {
   Object.freeze(state);
   switch (action.type) {
-    case RECEIVE_CURRENT_USER:
-      const currentUser = action.payload ? action.payload.currentUser : null;
-      return merge({}, { currentUser });
+    // case RECEIVE_CURRENT_USER:
+    //   const currentUser = action.payload ? action.payload.currentUser : null;
+    //   return merge({}, { currentUser });
     default:
       return state;
   }
