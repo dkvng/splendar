3e4bc7e0c2c6872d006a9de2bc972bf7088fedf6
Dan Kang <dankang27@gmail.com>
2018-06-21 13:25:26 -0400

    set up frontend

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/splendar.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
